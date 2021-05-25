52373fc399d2b714412bfb8a41fe05eb87bc77e5
ekernf01 <eric.kern13@gmail.com>
2021-05-25 10:49:54 -0400

    Pass test for loading and assembling compact LOOKs in Julia, woot

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using jlookc
 using Test
+using DelimitedFiles
 
 @testset "jlookc.jl" begin
-    # Write your tests here.
+
+  # Just one integration test: does it get the same answer as the R code?
+  println(pwd())
+  looks2 = jlookc.loadCompactLooks( "../data/test_save_looks")
+  from_compact = jlookc.formAllLooks(looks2["knockoffs"], looks2["vars_to_omit"], looks2["updates"])
+  from_r = [readdlm("../data/test_save_looks_full/" * string(i) * ".csv", ',', header = true)[1] for i in 1:10]
+  for k in 1:length(from_r)
+    @test sum( broadcast(abs, from_compact[k] - from_r[k] ) ) < 1e-10
+  end
+
 end
---
+++ test/runtests.jl~
@@ -0,0 +1,6 @@
+using jlookc
+using Test
+
+@testset "jlookc.jl" begin
+    # Write your tests here.
+end
